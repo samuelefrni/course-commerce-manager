afdbc1bf17733f80d47c93008584621ab8d1a094
Samuele Furnari <samuelefurnari9@gmail.com>
2023-12-25 12:44:28 +0100

    Second Commit

--- CourseCommerceManager.sol
+++ CourseCommerceManager.sol
@@ -1,11 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-// library CourseCommerceLibrary {
-// }
+library CourseCommerceLibrary {
+    struct Purchase {
+        string name;
+    }
+
+    function getPurchase(
+        address _customer,
+        CourseCommerceManager.Sale[] storage _allSales
+    ) internal view returns (Purchase[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < _allSales.length; i++) {
+            if (_allSales[i].buyer == _customer) {
+                count++;
+            }
+        }
+
+        Purchase[] memory purchases = new Purchase[](count);
+        count = 0;
+        for (uint256 i = 0; i < _allSales.length; i++) {
+            if (_allSales[i].buyer == _customer) {
+                purchases[count] = Purchase(_allSales[i].name);
+                count++;
+            }
+        }
+
+        return purchases;
+    }
+
+    function getEtherAmount(
+        uint256 _initialPurchaseDate,
+        uint256 _finalPurchaseDate,
+        CourseCommerceManager.Sale[] storage _allSales
+    ) internal view returns (uint256) {
+        require(
+            _initialPurchaseDate < _finalPurchaseDate,
+            "Initial purchase date must be greater than the final purchase date"
+        );
+        uint256 totalEtherAmount = 0;
+
+        for (uint256 i = 0; i < _allSales.length; i++) {
+            if (
+                _allSales[i].purchaseDate >= _initialPurchaseDate &&
+                _allSales[i].purchaseDate <= _finalPurchaseDate
+            ) {
+                totalEtherAmount += 1 ether;
+            }
+        }
+        return totalEtherAmount;
+    }
+}
 
 contract CourseCommerceManager {
-    address payable public owner;
+    address public owner;
     uint256 public numberSales;
     Product[] public allCourses;
     Sale[] public allSales;
@@ -25,10 +73,10 @@ contract CourseCommerceManager {
 
     Product englishCourse =
         Product({
-            id: 24810,
+            id: 2468,
             productAddress: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
             name: "English Course",
-            price: 1 ether
+            price: 5000000 gwei
         });
 
     Product italianCourse =
@@ -36,15 +84,24 @@ contract CourseCommerceManager {
             id: 36912,
             productAddress: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
             name: "Italian Course",
-            price: 1 ether
+            price: 5000000 gwei
         });
 
     modifier onlyOwner() {
-        require(owner == msg.sender);
+        require(
+            owner == msg.sender,
+            "You don't have the permission to execute this function!"
+        );
         _;
     }
 
     event saleAdded(string name, uint256 purchaseDate, address buyer);
+
+    event etherAmountFromTo(
+        uint256 startTime,
+        uint256 endTime,
+        uint256 saleAmount
+    );
 
     constructor() {
         owner = payable(msg.sender);
@@ -52,10 +109,17 @@ contract CourseCommerceManager {
         allCourses.push(italianCourse);
     }
 
-    function withdraw() public payable onlyOwner {
+    function withdraw(uint256 _amount, address payable _toAddress)
+        public
+        payable
+        onlyOwner
+    {
         uint256 contractBalance = address(this).balance;
-        require(contractBalance > 0, "Nothing to withdraw");
-        owner.transfer(contractBalance);
+        require(
+            contractBalance >= _amount,
+            "You probably don't have the funds to effect this withdraw, check the amount and try again!"
+        );
+        _toAddress.transfer(_amount);
     }
 
     function buyCourse(uint256 _idCourse) public payable {
@@ -103,7 +167,11 @@ contract CourseCommerceManager {
         );
     }
 
-    function searchId(uint256 _idProduct) public view returns (string memory) {
+    function searchById(uint256 _idProduct)
+        public
+        view
+        returns (string memory)
+    {
         for (uint256 i = 0; i < allCourses.length; i++) {
             if (allCourses[i].id == _idProduct) {
                 return allCourses[i].name;
@@ -112,15 +180,49 @@ contract CourseCommerceManager {
         revert("ID product not found");
     }
 
-    function checkPurchase(address _addressBuyer) public view returns (string[] memory) {
-        string[] memory purchase = new string[](allSales.length);
+    function checkPurchase(address _addressBuyer)
+        public
+        view
+        returns (CourseCommerceLibrary.Purchase[] memory)
+    {
+        return CourseCommerceLibrary.getPurchase(_addressBuyer, allSales);
+    }
 
-        for(uint256 i = 0; i < allSales.length; i++) {
-            if(allSales[i].buyer == _addressBuyer) {
-                purchase[i] = allSales[i].name;
+    function getSalesEtherAmountFromDate(
+        uint256 _initialPurchaseDate,
+        uint256 _finalPurchaseDate
+    ) public returns (uint256) {
+        uint256 totalEtherAmount = CourseCommerceLibrary.getEtherAmount(
+            _initialPurchaseDate,
+            _finalPurchaseDate,
+            allSales
+        );
+
+        emit etherAmountFromTo(
+            _initialPurchaseDate,
+            _finalPurchaseDate,
+            totalEtherAmount
+        );
+
+        return totalEtherAmount;
+    }
+
+    function searchSaleByPurchaseDate(uint256 _purchaseDate, address _buyer)
+        public
+        view
+        returns (string memory)
+    {
+        string memory sale;
+
+        for (uint256 i = 0; i < allSales.length; i++) {
+            if (
+                allSales[i].purchaseDate == _purchaseDate &&
+                allSales[i].buyer == _buyer
+            ) {
+                sale = allSales[i].name;
             }
-        }  
-        require(purchase.length > 0, "This address does buy anithings");
-        return purchase;
+        }
+
+        return sale;
     }
 }
